2e15843ce75fe67b8ffca2b7efbbcdc861129627
Alec Ritson <alec@neondigital.co.uk>
2018-08-30 16:48:09 +0100

    Handle webhooks

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -10,6 +10,8 @@ use Illuminate\Routing\Controller;
 use Braintree\WebhookNotification;
 use App\Services\HomingpinService;
 use Symfony\Component\HttpFoundation\Response;
+use App\Events\UserSubscriptionRefreshedEvent;
+use App\Services\Notifications\OutgoingService;
 
 class WebhookController extends Controller
 {
@@ -29,11 +31,35 @@ class WebhookController extends Controller
 
         $method = 'handle'.studly_case(str_replace('.', '_', $webhook->kind));
 
+        $subscription = $this->getSubscriptionById($webhook->subscription->id);
+
         if (method_exists($this, $method)) {
-            return $this->{$method}($webhook);
+            $result = $this->{$method}($webhook);
         } else {
-            return $this->missingMethod();
+            $result = $this->missingMethod();
         }
+
+
+        return $result;
+    }
+
+    public function generateTest(Request $request)
+    {
+        /**
+         * Available test codes:
+         *
+         * subscription_went_past_due
+         * subscription_canceled
+         * subscription_charged_unsuccessfully
+         * subscription_charged_successfully
+         * subscription_expired
+         */
+        $sampleNotification = \Braintree\WebhookTesting::sampleNotification(
+            $request->type,
+            $request->braintree_id
+        );
+
+        return response()->json($sampleNotification);
     }
 
     /**
@@ -47,16 +73,63 @@ class WebhookController extends Controller
         return WebhookNotification::parse($request->bt_signature, $request->bt_payload);
     }
 
+    protected function handleSubscriptionChargedUnsuccessfully($webhook)
+    {
+        $subscription = $this->getSubscriptionById($webhook->subscription->id);
+        $user = $subscription->getUser();
+
+        $notification = new OutgoingService;
+        $notification->addUser($user);
+        $notification->setSubject(
+            trans(
+                'notifications/email/subscription_payment_failed.subject',
+                [],
+                null,
+                $user->getLocale()
+            )
+        );
+        $notification->sendEmail('subscription_payment_failed');
+
+        return new Response('Webhook Handled', 200);
+    }
+
+    /**
+     * Handle the request for a successful payment
+     *
+     * @param mixed $webhook
+     * @return void
+     */
     protected function handleSubscriptionChargedSuccessfully($webhook)
     {
         $subscription = $this->getSubscriptionById($webhook->subscription->id);
         $user = $subscription->getUser();
 
-        // Update the users HomingPINs
+        // If the user isn't subscribed, resume.
+        if (!$user->subscribed('HomingPIN') && $user->subscription('HomingPIN')->onGracePeriod()) {
+            $user->subscription('HomingPIN')->resume();
+        }
+
+        // Update the users HomingPINs to the next billing date
         (new HomingpinService)->refreshHomingPinsForUser(
             $user,
-            $webhook->subscription->nextBillingDate
+            $subscription->getDetails()->nextBillingDate
         );
+
+        $notification = new OutgoingService;
+        $notification->addUser($user);
+        $notification->setContent([
+            'next_renewal' => $subscription->getDetails()->nextBillingDate,
+        ]);
+
+        $notification->setSubject(
+            trans(
+                'notifications/email/subscription_renewed.subject',
+                [],
+                null,
+                $user->getLocale()
+            )
+        );
+        $notification->sendEmail('subscription_renewed');
 
         return new Response('Webhook Handled', 200);
     }
@@ -92,8 +165,23 @@ class WebhookController extends Controller
     protected function cancelSubscription($subscriptionId)
     {
         $subscription = $this->getSubscriptionById($subscriptionId);
+        $user = $subscription->getUser();
 
         if ($subscription && (! $subscription->cancelled() || $subscription->onGracePeriod())) {
+
+            $notification = new OutgoingService;
+            $notification->addUser($user);
+
+            $notification->setSubject(
+                trans(
+                    'notifications/email/subscription_cancelled.subject',
+                    [],
+                    null,
+                    $user->getLocale()
+                )
+            );
+            $notification->sendEmail('subscription_cancelled');
+
             $subscription->markAsCancelled();
         }
 
